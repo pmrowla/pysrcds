139fb80876f58ee9266fbffebde92a8492240955
Peter Rowlands <peter@pmrowla.com>
2013-01-04 13:30:11 -0600

    initial setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pysrcds',
+      version='0.0.0',
+      description='Python library for interacting with Source engine dedicated servers',
+      author='Peter Rowlands',
+      author_email='peter@pmrowla.com',
+      url='',
+      packages=['srcds'],
+     )

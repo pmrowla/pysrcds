53e358435ab02d6a76580d53121e6865d6b794e7
Peter Rowlands <peter@pmrowla.com>
2013-01-08 21:19:33 -0600

    add travis-ci config and update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ from distutils.core import setup
 
 setup(name='pysrcds',
       version='0.0.0',
-      description='Python library for interacting with Source engine dedicated servers',
+      description='Python library for interacting with Source engine dedicated'
+                  ' servers',
       author='Peter Rowlands',
       author_email='peter@pmrowla.com',
       url='',
       packages=['srcds'],
-     )
+      )

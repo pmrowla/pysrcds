b4d2fb7a72530622a9dcbd21ca3813de3e6440b0
Peter Rowlands <peter@pmrowla.com>
2013-01-26 12:42:45 -0600

    increment bugfix

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
         return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='pysrcds',
-      version='0.0.3.alpha',
+      version='0.0.4.alpha',
       description='Python library for interacting with Source engine dedicated'
                   ' servers',
       long_description=read('README.rst'),

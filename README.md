562ee75809ff37316e48d1f8242804a9b43d9ad3
Peter Rowlands <peter@pmrowla.com>
2013-01-31 21:07:01 -0600

    increment version to 0.1.0 and update readme

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
-=======
 pysrcds
 =======
 
 Python library for interacting with Source engine dedicated servers.
 
-|BuildStatus|_
-
-.. |BuildStatus| image:: https://travis-ci.org/pmrowla/pysrcds.png
-.. _BuildStatus: https://travis-ci.org/pmrowla/pysrcds
+[![Build Status](https://travis-ci.org/pmrowla/pysrcds.png)](https://travis-ci.org/pmrowla/pysrcds)
 
 pysrcds provides the functionality to communicate with a dedicated server via
 RCON and also provides the ability to parse Source engine logs. There are also
@@ -15,9 +11,8 @@ some utility classes that may be useful for developing other Source related
 functionality.
 
 License
-=======
+-------
 
-pysrcds is distributed under the MIT license. See LICENSE.rst_ for
-more information.
-
-.. _LICENSE.rst: https://github.com/pmrowla/pysrcds/blob/master/LICENSE.rst
+pysrcds is distributed under the MIT license. See
+[LICENSE.md](https://github.pmrowla.com/pysrcds/blob/master/LICENSE.md)
+for more information.
--- setup.py
+++ setup.py
@@ -7,20 +7,37 @@ def read(fname):
         return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='pysrcds',
-      version='0.0.5.alpha',
+      version='0.1.0',
       description='Python library for interacting with Source engine dedicated'
                   ' servers',
-      long_description=read('README.rst'),
       author='Peter Rowlands',
       author_email='peter@pmrowla.com',
       url='https://github.com/pmrowla/pysrcds',
       packages=['srcds', 'srcds/events'],
       classifiers=[
-          'Development Status :: 2 - Pre-Alpha',
+          'Development Status :: 4 - Beta',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: MIT License',
           'Programming Language :: Python',
           'Topic :: Games/Entertainment :: First Person Shooters',
           'Topic :: Software Development :: Libraries :: Python Modules',
      ]
+     long_description= \
+'''
+=======
+pysrcds
+=======
+
+Python library for interacting with Source engine dedicated servers.
+
+pysrcds provides the functionality to communicate with a dedicated server via
+RCON and also provides the ability to parse Source engine logs. There are also
+some utility classes that may be useful for developing other Source related
+functionality.
+
+License
+=======
+
+pysrcds is distributed under the MIT license.
+''',
     )

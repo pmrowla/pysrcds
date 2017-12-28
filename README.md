5a714c5f33410d14cd953cde937dde409934e2f9
Peter Rowlands <peter@pmrowla.com>
2017-12-28 11:51:58 +0900

    update travis & install reqs

--- README.md
+++ README.md
@@ -5,8 +5,7 @@ Python library for interacting with Source engine dedicated servers.
 
 [![Build Status](https://travis-ci.org/pmrowla/pysrcds.png)](https://travis-ci.org/pmrowla/pysrcds)
 [![Coverage Status](https://coveralls.io/repos/pmrowla/pysrcds/badge.png)](https://coveralls.io/r/pmrowla/pysrcds)
-[![PyPi version](https://pypip.in/v/pysrcds/badge.png)](https://crate.io/packages/pysrcds/)
-[![PyPi downloads](https://pypip.in/d/pysrcds/badge.png)](https://crate.io/packages/pysrcds/)
+[![PyPI version](https://badge.fury.io/py/pysrcds.svg)](https://pypi.python.org/pypi/pysrcds/)
 
 pysrcds provides the functionality to communicate with a dedicated server via
 RCON and also provides the ability to parse Source engine logs. There are also
--- setup.py
+++ setup.py
@@ -1,29 +1,26 @@
 #!/usr/bin/env python
 
-import os
 from distutils.core import setup
 
-def read(fname):
-        return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-setup(name='pysrcds',
-      version='0.1.3',
-      description='Python library for interacting with Source engine dedicated'
-                  ' servers',
-      author='Peter Rowlands',
-      author_email='peter@pmrowla.com',
-      url='https://github.com/pmrowla/pysrcds',
-      packages=['srcds', 'srcds/events'],
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python',
-          'Topic :: Games/Entertainment :: First Person Shooters',
-          'Topic :: Software Development :: Libraries :: Python Modules',
-     ],
-     long_description=\
-'''
+setup(
+    name='pysrcds',
+    version='0.1.5',
+    description='Python library for interacting with Source engine dedicated'
+                ' servers',
+    author='Peter Rowlands',
+    author_email='peter@pmrowla.com',
+    url='https://github.com/pmrowla/pysrcds',
+    packages=['srcds', 'srcds/events'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Games/Entertainment :: First Person Shooters',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requres=['future'],
+    long_description='''
 =======
 pysrcds
 =======
@@ -40,4 +37,4 @@ License
 
 pysrcds is distributed under the MIT license.
 ''',
-    )
+)

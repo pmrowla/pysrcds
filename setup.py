cf4caa6abd7756e76ab3e613b183f163fd8d88cc
Peter Rowlands <peter@pmrowla.com>
2017-12-29 14:30:29 +0900

    increment version [ci skip]

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='pysrcds',
-    version='0.1.5',
+    version='0.1.6.dev',
     description='Python library for interacting with Source engine dedicated'
                 ' servers',
     author='Peter Rowlands',
